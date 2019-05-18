6b9b954a96efc4566a5e2ea78037f7aa2a037f7b
Isabela Rabasa <isabelarabasa@Isabelas-MacBook-Pro.local>
2019-05-17 22:08:02 -0500

    third commit

--- liri.js
+++ liri.js
@@ -1,20 +1,58 @@
 // enviromental variables with the dotenv package
 require("dotenv").config();
 
-var keys = require("./keys.js");
-var Spotify = require("node-spotify-api");
-var Spotify = new Spotify(keys.spotify);
+var request = require("request");
+var moment = require("moment");
 var fs = require("fs");
+var keys = require("./keys.js");
+var spotify = require("node-spotify-api");
+// var spotify = new Spotify(keys.spotify);
 
-var userOption = process.argv[2];
-var input = process.argv[3];
+var command = process.argv[2];
+var input = JSON.stringify(process.argv[3]);
 
-Spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+function userCommand(command, input) {
+switch (command) {
+  case "concert-this":
+    concertThis(input);
+    break;
+  case "spotify-this-song":
+    spotifyThisSong(input);
+    break;
+  case "movie-this":
+    movieThis(input);
+    break;
+  default:
+    console.log("Please enter one of the following commands: 'concert-this', 'spotify-this-song', 'movie-this' ");
+    break;
+  }
+};
+
+userCommand(command, input); 
+
+
+
+
+// search Spotify for songs function
+function spotifyThisSong(songName) {
+  var spotify = new Spotify(keys.spotify);
+  spotify.search({ type: 'track', query: input }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
-    }
-   
+    }  
   console.log(data); 
   });
+};
 
- userInput(userOption, inputParameter);
+
+
+
+
+// Bands in Town for concerts function
+function concertthis(bandQuery) {
+
+  
+}
+
+
+
